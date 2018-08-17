209fd1fd25e8750d88fcaecec2f011a0016d58f8
Laura Kulas <laura.kulas@gmail.com>
2018-08-16 20:09:41 -0700

    added POST endpoint at /blog-posts

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -24,4 +24,18 @@ router.get('/', (req, res) => {
 	res.json(BlogPosts.get());
 });
 
+router.post('/', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author'];
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`{field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	const post = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	res.status(201).json(post);
+});
+
 module.exports = router;
